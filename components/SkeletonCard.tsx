7dc28ce0f70bb1f979a0c833517c854af511f03c
deflex82 <susantrimal77@gmail.com>
2024-06-14 17:43:16 +0545

    d

--- components/SkeletonCard.tsx
+++ components/SkeletonCard.tsx
@@ -3,10 +3,10 @@ import { Skeleton } from "@/components/ui/skeleton"
 export function SkeletonCard() {
   return (
     <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[125px] w-[300px] rounded-xl" />
+      <Skeleton className="h-[125px] w-full lg:w-[300px] rounded-xl" />
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+        <Skeleton className="h-4 lg:w-[300px] w-full " />
+        <Skeleton className="h-4 lg:w-[300px] w-full" />
       </div>
     </div>
   )
